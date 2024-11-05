5424433e66e136dcc423a6e82a28e077c45a771d
cap.m.roohani <cap.m.roohani@outlook.com>
2024-11-05 09:41:42 +0330

    install and config dependencies (Tailwind, NuxtIcon)

--- src/module.ts
+++ src/module.ts
@@ -1,7 +1,8 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { defineNuxtModule, addPlugin, createResolver, addComponentsDir, installModule } from '@nuxt/kit'
 
 // Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface ModuleOptions {
+}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
@@ -10,10 +11,33 @@ export default defineNuxtModule<ModuleOptions>({
   },
   // Default configuration options of the Nuxt module
   defaults: {},
-  setup(_options, _nuxt) {
+  async setup(_options, nuxt) {
     const resolver = createResolver(import.meta.url)
+    nuxt.options.modules.push('@nuxtjs/tailwindcss', '@nuxt/icon')
+
+    nuxt.hook('nitro:config', async (nitroConfig) => {
+      // if not already available, intialize as empty array
+      nitroConfig.publicAssets ||= []
+      nitroConfig.publicAssets.push({
+        dir: resolver.resolve('./runtime/assets'), // path of assets
+        maxAge: 60 * 60 * 24 * 365, // cache assets for 1 year
+      })
+    })
+
+    await installModule('@nuxtjs/tailwindcss', {
+      configPath: resolver.resolve('./runtime/tailwind.config.js'),
+    })
+    await installModule('@nuxt/icon')
+
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
     addPlugin(resolver.resolve('./runtime/plugin'))
+
+    await addComponentsDir({
+      path: resolver.resolve('./runtime/components'), // path of components
+      pathPrefix: false, // Prefix component name by its path.
+      prefix: '', // Prefix all matched components.
+      global: true, // Registers components to be globally available.
+    })
   },
 })
